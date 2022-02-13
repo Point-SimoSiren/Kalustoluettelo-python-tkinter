79136342aa034915e7402f3f6ce90d69276b31a5
Simo Siren <sirensimo7@gmail.com>
2022-02-13 18:09:07 +0200

    buttons

--- tool_manager.py
+++ tool_manager.py
@@ -8,6 +8,22 @@ app.geometry('700x500')
 tool_list = Listbox(app, width=100, height=20, border=1)
 tool_list.grid(column=0, row=0, columnspan=5, rowspan=6, pady=5, padx=5)
 
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=0, column=5)
+
+tool_list.configure(yscrollcommand = scrollbar.set)
+scrollbar.configure(command=tool_list.yview)
+
+# Napit
+add_btn = Button(app, text='Luo uusi', width=12)
+add_btn.grid(row=11, column=2)
+
+add_btn = Button(app, text='Tallenna', width=12)
+add_btn.grid(row=11, column=3)
+
+add_btn = Button(app, text='Poista', width=12)
+add_btn.grid(row=11, column=4)
+
 # Tavara
 tool_text = StringVar()
 tool_label = Label(app, text='Tavara', font=('bold', 14), pady=5)
